7f912fbb21bb96eab0ca10d4ea56ca06e38c65e7
jareerzeenam <jareerzeenam.28@gmail.com>
2020-09-11 17:31:05 +0530

    Styles Done working on the JS functionalities

---
+++ app.js
@@ -0,0 +1,44 @@
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
+
+  //   ! Start the game
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+  //   ! Play match
+  const playMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
+
+    // ! Computer Options
+    const computerOptions = ["rock", "paper", "scissors"];
+
+    options.forEach((option) => {
+      option.addEventListener("click", function () {
+        // ! Computer Choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+        console.log(computerChoice);
+      });
+    });
+
+    // console.log(computerNumber);
+  };
+
+  // ! Call all the inner functions
+  startGame();
+  playMatch();
+};
+
+game();
+
+// ! Start the game function
